c45d52f6556509746b684c76c0651e5ecd93c56e
Xincheng <ye_xcheng@163.com>
2023-11-20 17:42:28 +1000

    assert shape

--- metrics.py
+++ metrics.py
@@ -13,6 +13,10 @@ def compute_dice_coefficient(mask_gt, mask_pred):
   Returns:
     the dice coeffcient as float. If both masks are empty, the result is NaN
   """
+  # assert mask_gt.shape == mask_pred.shape
+  assert len(mask_gt.shape)==3 and len(mask_pred.shape)==3, 'should be 3 channels'
+  assert mask_gt.shape[0]==mask_pred.shape[0] and mask_gt.shape[1]==mask_pred.shape[1] and mask_gt.shape[2]==mask_pred.shape[2], 'image and ground truth should have the same size'
+            
   volume_sum = mask_gt.sum() + mask_pred.sum()
   if volume_sum == 0:
     return np.NaN
