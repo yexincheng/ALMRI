22ec88c56aad044670826a0262dd75048a130a32
Xincheng <ye_xcheng@163.com>
2024-01-08 14:45:36 +1000

    add random seed

--- strategy.py
+++ strategy.py
@@ -2,9 +2,12 @@ import logging
 import random
 import time
 import numpy as np
+from utils import NpzDataset
+from torch.utils.data import DataLoader
+from eval import infer
 
-def random_sampling(training_pool, sample_pool):
-    np.random.seed(2023)
+def random_sampling(training_pool, sample_pool, seed):
+    np.random.seed(seed)
     next_sample = np.random.choice(training_pool)
     sample_pool.append(next_sample)
     training_pool.remove(next_sample)
