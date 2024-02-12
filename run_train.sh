208e8435c61da89786ef600081f07ff18d68cf03
yexincheng <ye_xcheng@163.com>
2024-02-12 19:37:51 +1000

    dyn nocache plot 5 seeds

--- run_train.sh
+++ run_train.sh
@@ -1,14 +1,15 @@
 #!/bin/bash --login
 #SBATCH --nodes 1
-#SBATCH --job-name=sam_e20random2024
+#SBATCH --job-name=samroi_e20random2024
 #SBATCH --ntasks 1
 #SBATCH -c 30
 #SBATCH --mem=50000
-#SBATCH -o results/logs/sam_e20random2024_out.txt
-#SBATCH -e results/logs/sam_e20random2024_error.txt
+#SBATCH -o results/logs/samroi_e20random2024_out.txt
+#SBATCH -e results/logs/samroi_e20random2024_error.txt
 #SBATCH --partition=gpu_cuda
 #SBATCH --gres=gpu:1
-#sbatch --account=a_barth
+#SBATCH --account=a_barth
+#SBATCH --time=34:00:00
 
 module load cuda/11.7.0
 # module load gnu/5.4.0
@@ -21,7 +22,7 @@ source activate medsam
 # wandb login --relogin
 srun python train.py --task MRI_LeftKidney \
     --label_id 1 \
-    --base_model SAM \
+    --base_model SAM_roi \
     --checkpoint ./checkpoints/SAM/sam_vit_b_01ec64.pth \
     --num_epochs 20 \
     --seed 2024
